586809fd13fbe58de3c2d083f0b627decd2fa21f
emjo91 <3mle33@gmail.com>
2018-08-31 11:10:41 -0500

    babel is FINALLY working

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+// include the js minification plugin
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  entry: ['./js/src/app.js'],
+  output: {
+    filename: './js/build/app.min.js',
+    path: path.resolve(__dirname)
+  },
+  module: {
+    rules: [
+      // perform js babelization on all .js files
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+          presets: ['babel-preset-env']
+          }
+        }
+      }
+    ]
+  },
+  optimization: {
+    minimizer: [
+      // enable the js minification plugin
+      new UglifyJSPlugin({
+        cache: true,
+        parallel: true
+      })
+    ]
+  }
+};
